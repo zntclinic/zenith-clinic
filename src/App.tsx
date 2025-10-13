29e0e23a284e92c4190f16f89965564e73e9325d
Andrea Sánchez Blanco <andreasanchezblanco@gmail.com>
2025-10-13 14:17:56 +0200

    Fix npm run dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Index from "./pages/Index";
 import Tratamientos from "./pages/Tratamientos";
 import SobreNosotros from "./pages/SobreNosotros";
@@ -11,7 +11,7 @@ const App = () => (
   <TooltipProvider>
     <Toaster />
     <Sonner />
-    <BrowserRouter basename="/zenith-clinic">
+    <BrowserRouter basename={import.meta.env.PROD ? "/zenith-clinic" : "/"}>
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/tratamientos" element={<Tratamientos />} />
