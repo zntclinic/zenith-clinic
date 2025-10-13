93b37fa36f34593b9c0f406d5507c50bbd2a1e70
Andrea Sánchez Blanco <andreasanchezblanco@gmail.com>
2025-10-13 14:18:05 +0200

    Reset scroll on page change

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,29 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import Index from "./pages/Index";
 import Tratamientos from "./pages/Tratamientos";
 import SobreNosotros from "./pages/SobreNosotros";
 import NotFound from "./pages/NotFound";
+
+// Component to scroll to top on route change
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
 
 const App = () => (
   <TooltipProvider>
     <Toaster />
     <Sonner />
     <BrowserRouter basename={import.meta.env.PROD ? "/zenith-clinic" : "/"}>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/tratamientos" element={<Tratamientos />} />
