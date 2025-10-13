c27a19f63f9e3acdb8a93f9c27134db46bfd3fe7
Andrea Sánchez Blanco <andreasanchezblanco@gmail.com>
2025-10-13 16:44:33 +0200

    Revert "Refactor code to Lovable standards"
    This reverts commit 7c8061cf974e95709ba60bfa2d95ac38ca482d1c.

--- src/components/icons/SocialIcons.tsx
+++ src/components/icons/SocialIcons.tsx
@@ -1,17 +1,25 @@
-import { Instagram, Mail, MessageCircle } from "lucide-react";
+import InstagramSvg from "@/assets/icons/instagram.svg?react";
+import GmailSvg from "@/assets/icons/gmail.svg?react";
+import WhatsAppSvg from "@/assets/icons/whatsapp.svg?react";
 
 interface IconProps {
   className?: string;
 }
 
-export const InstagramIcon = ({ className = "w-4 h-4" }: IconProps) => {
-  return <Instagram className={className} />;
+export const InstagramIcon = ({ className = "w-4 h-4 text-black" }: IconProps) => {
+  return (
+    <InstagramSvg className={className} />
+  );
 };
 
-export const GmailIcon = ({ className = "w-4 h-4" }: IconProps) => {
-  return <Mail className={className} />;
+export const GmailIcon = ({ className = "w-4 h-4 text-black" }: IconProps) => {
+  return (
+    <GmailSvg className={className} />
+  );
 };
 
-export const WhatsAppIcon = ({ className = "w-4 h-4" }: IconProps) => {
-  return <MessageCircle className={className} />;
+export const WhatsAppIcon = ({ className = "w-4 h-4 text-black" }: IconProps) => {
+  return (
+    <WhatsAppSvg className={className} />
+  );
 };
